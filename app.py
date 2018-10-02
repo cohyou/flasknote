33c352e3e89b5e6bd167ca61a4393abba71e74a7
cohyou <cohyou@ironoir.io>
2018-10-02 18:06:58 +0900

    add form

--- app.py
+++ app.py
@@ -1,31 +1,54 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, url_for
 from hamlish_jinja import HamlishExtension
 from werkzeug import ImmutableDict
 import os
-import sqlite3
+# import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 class FlaskWithHamlish(Flask):
     jinja_options = ImmutableDict(extensions=[HamlishExtension])
 
 app = FlaskWithHamlish(__name__)
 
+db_uri = 'sqlite:///' + os.path.join(app.root_path, 'flasknote.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Entry(db.Model):
+    __tablename__ = 'entries'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+
 @app.route('/')
 def hello_world():
-    entries = get_db().execute('SELECT title, body FROM entries').fetchall()
+    # entries = get_db().execute('SELECT title, body FROM entries').fetchall()
+    entries = Entry.query.all()
     return render_template('index.haml', entries=entries)
 
-def connect_db():
-    db_path = os.path.join(app.root_path, 'flasknote.db')
-    rv = sqlite3.connect(db_path)
-    rv.row_factory = sqlite3.Row
-    return rv
+@app.route('/post', methods=['POST'])
+def add_entry():
+    entry = Entry()
+    entry.title = request.form['title']
+    entry.body = request.form['body']
+    db.session.add(entry)
+    db.session.commit()
+    
+    return redirect(url_for('hello_world'))
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+# def connect_db():
+#     db_path = os.path.join(app.root_path, 'flasknote.db')
+#     rv = sqlite3.connect(db_path)
+#     rv.row_factory = sqlite3.Row
+#     return rv
 
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+# def get_db():
+#     if not hasattr(g, 'sqlite_db'):
+#         g.sqlite_db = connect_db()
+#     return g.sqlite_db
+
+# @app.teardown_appcontext
+# def close_db(error):
+#     if hasattr(g, 'sqlite_db'):
+#         g.sqlite_db.close()

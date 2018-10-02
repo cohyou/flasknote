1ab5afcf1f80fe1110b1e3adf8f249850cb12687
cohyou <cohyou@ironoir.io>
2018-10-02 17:21:43 +0900

    second commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, g
+from hamlish_jinja import HamlishExtension
+from werkzeug import ImmutableDict
+import os
+import sqlite3
+
+class FlaskWithHamlish(Flask):
+    jinja_options = ImmutableDict(extensions=[HamlishExtension])
+
+app = FlaskWithHamlish(__name__)
+
+@app.route('/')
+def hello_world():
+    entries = get_db().execute('SELECT title, body FROM entries').fetchall()
+    return render_template('index.haml', entries=entries)
+
+def connect_db():
+    db_path = os.path.join(app.root_path, 'flasknote.db')
+    rv = sqlite3.connect(db_path)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
